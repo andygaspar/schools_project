f67ea39f64827e00a3e02ea9dc1bae826160a4ac
andgaspar <andrea.gasparin.87@gmail.com>
2023-01-25 08:01:14 +0100

    sistemati grafici

--- FL/run_fl.py
+++ FL/run_fl.py
@@ -5,11 +5,11 @@ from FL.fl import FacilityLocation
 import networkx as nx
 
 f = FacilityLocation()
-f.draw('Pictures/fl.png')
-# f.check_and_draw_solution()
-# f.draw_csv_solutions()
-f.g.g
-MAT = nx.to_numpy_array(f.g.g, weight='weight')
-streets = np.nonzero(MAT)
-
-dist = pd.DataFrame({'points': zip(streets[0], streets[1]),'dist': MAT[streets]})
+f.draw()
+f.check_and_draw_solution()
+f.draw_csv_solutions()
+# f.g.g
+# MAT = nx.to_numpy_array(f.g.g, weight='weight')
+# streets = np.nonzero(MAT)
+#
+# dist = pd.DataFrame({'points': zip(streets[0], streets[1]),'dist': MAT[streets]})
